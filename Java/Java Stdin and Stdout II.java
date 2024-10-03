f77305b5fbcb4c7263fd44f0111daa17b3539243
Mohan Krishna <142295454+mohan13krishna@users.noreply.github.com>
2024-10-03 22:18:19 +0530

    commit

---
+++ Java/Java Stdin and Stdout II.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Solution { 
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+          int i = scanner.nextInt(); 
+          double d = scanner.nextDouble(); 
+          scanner.nextLine(); 
+          String s = scanner.nextLine(); 
+          System.out.println("String: " + s); 
+          System.out.println("Double: " + d); 
+          System.out.println("Int: " + i); 
+          }
+          }
