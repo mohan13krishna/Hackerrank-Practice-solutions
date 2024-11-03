2a7c2fa8ec6dcf4b7146e73ee02cfc8b04389f69
Mohan Krishna <142295454+mohan13krishna@users.noreply.github.com>
2024-11-03 16:20:49 +0530

    commit

---
+++ Java/Java Varargs - Simple Addition.java
@@ -0,0 +1,22 @@
+
+
+
+class Add {
+
+    void add(int a, int b) {
+        System.out.println(a + "+" + b + "=" + (a + b));
+    }
+
+    void add(int a, int b, int c) {
+        System.out.println(a + "+" + b + "+" + c + "=" + (a + b + c));
+    }
+
+    void add(int a, int b, int c, int d, int e) {
+        System.out.println(a + "+" + b + "+" + c + "+" + d + "+" + e + "=" + (a + b + c + d + e));
+    }
+
+    void add(int a, int b, int c, int d, int e, int f) {
+        System.out.println(a + "+" + b + "+" + c + "+" + d + "+" + e + "+" + f + "=" + (a + b + c + d + e + f));
+    }
+}
+
